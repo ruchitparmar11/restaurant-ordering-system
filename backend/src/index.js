df8745631f5049d05b796c8b67971b0a31f801db
ruchitparmar11 <ruchitparmar78@gmail.com>
2025-05-10 14:40:11 +0530

    This is the whole backend code

---
+++ backend/src/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+import authRoutes from "./routes/authRoutes.js";
+import { connectDB } from "./lib/db.js"
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+})
